0114b236b8fd78ca17e7fb021a75b88c4ce2ffcc
高橋芹奈 <84810159 +takanaaaa@users.noreply.github.com>
2021-11-21 12:28:06 +0900

    [Finish]java-6

---
+++ [JavaScript]chapter6/app.js
@@ -0,0 +1,19 @@
+//マウスオーバー、マウスアウト
+  // $(function(){
+  //   $('.box1').mouseover(function(){
+  //     $('.box1').addClass('box1-ext');
+  //   });
+  //   $('.box1').mouseout(function(){
+  //     $('.box1').removeClass('box1-ext');
+  //   })
+  // });
+
+//マウスクリック
+$(function(){
+  $('.box1').on('click', function(){
+    $('.box1').addClass('box1-ext');
+  });
+  $('.box1').mouseout(function(){
+    $('.box1').removeClass('box1-ext');
+  });
+});

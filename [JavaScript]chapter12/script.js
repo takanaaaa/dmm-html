ecde7835366190aeaf93ae36e76ddda5ee01540b
高橋芹奈 <84810159 +takanaaaa@users.noreply.github.com>
2021-11-21 21:35:58 +0900

    [Finish]java-12

---
+++ [JavaScript]chapter12/script.js
@@ -0,0 +1,32 @@
+const API_KEY = "dd9d2f38635cc7e36172d3ca843bcf45";
+
+$(function(){
+  $('#btn').on('click', function(){
+    //入力された都市名でWebAPIに天気情報をリクエスト
+    $.ajax({
+      url:"https://api.openweathermap.org/data/2.5/weather?q=" + $('#cityname').val() + "&units=metric&appid=" + API_KEY,
+      dataType : 'jsonp',
+    //通信成功（dataは任意の変数名）
+    }).done(function(data){
+      //位置
+      $('#place').text(data.name);
+      //最高気温
+      $('#temp_max').text(data.main.temp_max);
+      //最低気温
+      $('#temp_min').text(data.main.temp_min);
+      //湿度
+      $('#humidity').text(data.main.humidity);
+      //風速
+      $('#speed').text(data.wind.speed);
+      // 天気
+      $('#weather').text(data.weather[0].main);
+      // 天気アイコン
+      $('img').attr("src","http://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
+      $('img').attr("alt",data.weather[0].main);
+
+    //通信失敗（dataは任意の変数名）
+    }).fail(function(data){
+      alert('通信に失敗しました。')
+    });
+  });
+});
